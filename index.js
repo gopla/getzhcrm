a46b6ba08ef4d44f2e532e74c3e2c51c7bbddb8d
gopla <yudhistn@gmail.com>
2022-05-13 14:11:00 +0700

    Ubah ke server

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ require('dotenv').config()
 const zoho = require('@trifoia/zcrmsdk')
 const Deals = require('./models').Deals
 const cron = require('node-cron')
+const http = require('http')
 
 const config = require('./config/zoho.config')
 const { sequelize } = require('./models')
+const port = 3003
 
 // Get data from Zoho CRM by Module name and Page number
 let getDataFromCRM = async (pageNum, moduleName) => {
@@ -32,50 +34,59 @@ let deleteOneYearSP = async () => {
 	return data
 }
 
-// Main function scheduled
-// It'll run every day at 17:00
-cron.schedule('0 17 * * *', async () => {
-	// Setup variables
-	let pageNum = 1,
-		dealsData = '',
-		arrDeals = []
+// Create server
+http
+	.createServer(async (req, res) => {
+		cron.schedule('0 17 * * *', async () => {
+			console.log(` -> Application Running`)
+			// Setup variables
+			let pageNum = 1,
+				dealsData = '',
+				arrDeals = []
 
-	// Loop getDataFromCRM function as long as response has more_record
-	do {
-		dealsData = await getDataFromCRM(pageNum, 'Deals')
-		console.log(`Getting data at page ${pageNum}`)
-		dealsData.data
-			? console.log('Ada Data')
-			: console.log(`No Data? ${dealsData.data}`)
+			// Loop getDataFromCRM function as long as response has more_record
+			do {
+				dealsData = await getDataFromCRM(pageNum, 'Deals')
+				console.log(`Getting data at page ${pageNum}`)
+				dealsData.data
+					? console.log('Ada Data')
+					: console.log(`No Data? ${dealsData.data}`)
 
-		// Mapping response data to clean it and push it into array
-		dealsData.data.map((isiArray) => {
-			;(isiArray['Owner'] = isiArray['Owner'].name),
-				(isiArray['Product_2'] = isiArray.Product_2[0]),
-				(isiArray['Created_By'] = isiArray['Created_By'].name),
-				(isiArray['Modified_By'] = isiArray['Modified_By'].name),
-				(isiArray['Account_Name'] = isiArray['Account_Name'].name),
-				(isiArray['Contact_Name'] =
-					isiArray['Contact_Name'] == null
-						? null
-						: isiArray['Contact_Name'].name),
-				(isiArray['Tag'] =
-					isiArray.Tag[0] == null ? null : isiArray.Tag[0].name)
+				// Mapping response data to clean it and push it into array
+				dealsData.data.map((isiArray) => {
+					;(isiArray['Owner'] = isiArray['Owner'].name),
+						(isiArray['Product_2'] = isiArray.Product_2[0]),
+						(isiArray['Created_By'] = isiArray['Created_By'].name),
+						(isiArray['Modified_By'] = isiArray['Modified_By'].name),
+						(isiArray['Account_Name'] = isiArray['Account_Name'].name),
+						(isiArray['Contact_Name'] =
+							isiArray['Contact_Name'] == null
+								? null
+								: isiArray['Contact_Name'].name),
+						(isiArray['Tag'] =
+							isiArray.Tag[0] == null ? null : isiArray.Tag[0].name)
 
-			delete isiArray['$approval']
-			delete isiArray['$review_process']
+					delete isiArray['$approval']
+					delete isiArray['$review_process']
 
-			arrDeals.push(isiArray)
+					arrDeals.push(isiArray)
+				})
+				pageNum++
+			} while (dealsData.info.more_records)
+
+			// Bulk insert into Deals table by Array from before
+			await insertIntoTableDeals(arrDeals)
+
+			// Delete up to 1 year data
+			await deleteOneYearSP()
+
+			// Print how many data has been fetched into database
+			console.log(`${arrDeals.length} Deals Data Fetched`)
 		})
-		pageNum++
-	} while (dealsData.info.more_records)
 
-	// Bulk insert into Deals table by Array from before
-	await insertIntoTableDeals(arrDeals)
-
-	// Delete up to 1 year data
-	await deleteOneYearSP()
-
-	// Print how many data has been fetched into database
-	console.log(`${arrDeals.length} Deals Data Fetched`)
-})
+		res.write('Application running')
+		res.end()
+	})
+	.listen(port, () => {
+		console.log(` -> Hello World! Running on port ${port}`)
+	})
